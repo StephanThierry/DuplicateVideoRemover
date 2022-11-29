f61be2b6d28e7b9c251ad942b5080eef0cf620c0
HomeToken <stephan.thierry@live.com>
2022-11-29 19:20:11 +0100

    CleanupHandler

---
+++ CleanupHandler.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace deepduplicates
+{
+    public class CleanupHandler
+    {
+        private VideoInfoContext db { get; set; }
+        private FileHandler fh { get; set; }
+
+        public CleanupHandler(VideoInfoContext db, FileHandler fh){
+            this.db = db;
+            this.fh = fh;
+        }
+
+        public async Task runCleanup(){
+            int counter = 0;
+
+            Console.WriteLine("Resolving all files first...");
+            List<string> allFiles = fh.allFiles.ToList();
+            int removedEntries = 0;
+
+            Console.WriteLine("Reading all entried in DB.");
+            int dbEntries = db.VideoInfos.Count();
+            foreach (VideoInfo item in db.VideoInfos){
+                counter++;
+                
+                if (counter == 1 || counter % 100 == 0) Console.WriteLine(counter + "/"+ dbEntries + "  looking for: " + item.path);
+
+                string file = allFiles.FirstOrDefault(x =>x == item.path);
+                if (file == null)  {
+                    Console.WriteLine("NOT FOUND: " + item.path);
+                    removedEntries++;
+                    fh.deleteScreenshots(item);
+                    db.VideoInfos.Remove(item);
+                    Console.WriteLine("DB entry and screenshots deleted for id: " + item.id );
+                } else {
+                    allFiles.Remove(file);
+                }
+            }
+            await db.SaveChangesAsync();
+            Console.WriteLine("Removed number of entries: " + removedEntries);
+        }
+    }
+}
